7c0a1066d49478f2c8c297c18b5e02cae34999d4
MaryamArif7 <www.maryamarif28@gmail.com>
2024-05-01 14:11:19 +0500

    1

---
+++ index.js
@@ -0,0 +1,41 @@
+/* Redux:
+-> React only allows for a uni-directional flow of data. That means data cannot be sent from a child to a parent
+->in redux state cannot be mdified directly
+
+*/
+
+
+
+
+const redux = require('redux');
+
+const INITIAL_VALUE = {
+  counter: 0
+};
+
+const reducer = (store = INITIAL_VALUE, action) => {
+  let newStore = store;
+  if (action.type === 'INCREMENT') {
+    newStore = {counter: store.counter + 1};
+  } else if (action.type === 'DECREMENT') {
+    newStore = {counter: store.counter - 1};
+  } else if (action.type === 'ADDITION') {
+    newStore = {counter: store.counter + action.payload.number};
+  }
+  return newStore;
+}
+
+const store = redux.createStore(reducer);
+
+const subscriber = () => {
+  const state = store.getState();
+  console.log(state);
+}
+
+store.subscribe(subscriber);
+
+store.dispatch({type: 'INCREMENT'});
+store.dispatch({type: 'DECREMENT'});
+store.dispatch({type: 'INCREMENT'});
+store.dispatch({type: 'ADDITION', payload: {number: 7}});
+store.dispatch({type: 'DECREMENT'});
